0443766ccf2c2562aab8d41f287e93f04f21f133
Damian Mooyman <damo@brrltd.com>
2012-03-05 14:41:12 +1300

    General cleanup of code and bugfix. FIXED: Issue where saving image would leave behind file ADDED: Feature to toggle between deleting and leaving physical files when loading via CMS ADDED: Function for exporting images to file FIXED: Documentation UPDATED: General cleanup of messy code ADDED: Random file 'name' generation for database storage. FIXED: Issues where setting properties would trigger Image and File class functions that expected a physical file to be present. ADDED: CopyFromURL function to allow url importing of images. FIXED: Overrode unnecessary Image and File class functions that no longer were needed.

---
+++ README.md
@@ -0,0 +1,44 @@
+# Memory Image module for Silverstripe
+
+This module provides a simple way of storing and working with image data stored as BLOBs (Binary Large OBjects)
+stored in the database.
+
+The new MemoryImage class should pick up all Image class extensions that you may have defined, and can be used with all
+custom and built in image resizing mechanisms the same way as you would use a normal image.
+
+Images will not be deleted when doing a FilesystemSyncTask.
+
+## Credits and Authors
+
+ * Damian Mooyman - <https://github.com/tractorcow/silverstripe-lessphp>
+
+## Requirements
+
+ * SilverStripe 2.4.5, may work on lower versions
+ * PHP 5.2
+
+## Installation Instructions
+
+ * Extract all files into the 'memoryimage' folder under your Silverstripe root.
+ * Use the "MemoryImage" class in place of the "Image" class whenever you wish for an image to be stored in
+   memory instead of the filesystem.
+
+## API
+
+In addition to the MemoryImage class, this module also adds the Blob class for database storage of BLOB objects,
+and a MemoryGD class for providing image data manipulation of image data.
+
+The new MemoryImage includes these functions and properties
+
+### MemoryImage Properties
+
+ * Data - Allows image data string to be get/set
+ * DataBase64 - Same as above, but with base64 encoded data
+ 
+### MemoryImage Functions
+ * setData($data, $filename) - Allows the data property to be set, but with an explicit 'name' for storage in the database
+ * CopyFromImage(Image $image, $filename) - Copies the supplied image into this image
+ * CopyFromFile($path, $filename) - Copies the supplied file path into this image
+ * CopyFromURL($url, $filename) - Load content from the provided image URL into memory
+ * SaveToFile($path) - Writes the image data to the specified filesystem path
+
--- code/MemoryImage.php
+++ code/MemoryImage.php
@@ -1,100 +1,248 @@
 <?php
 
 /**
- * Stores files as blobs
- *
- * @author Damo
+ * Stores files as blobs and provides methods for importing or exporting images
+ * @property string $Data The image data as a binary string
+ * @property string $DataBase64 The image data as base 64 encoded string
+ * @author Damian Mooyman
  */
 class MemoryImage extends Image
 {
 
+    /**
+     * Determines whether uploaded images should be deleted from the file system after they have been stored in memory
+     * @var boolean
+     */
+    public static $remove_uploaded_files = true;
     static $db = array(
         'ImageData' => 'Blob'
     );
+    static $casting = array(
+        'Data' => 'HTMLText',
+        'DtaaBase64' => 'HTMLText'
+    );
 
-    // image header tables
+    /**
+     * Table mapping image types to the file data header
+     * @link http://www.garykessler.net/library/file_sigs.html
+     * @var array
+     */
     static $header_types = array(
-        // from http://www.garykessler.net/library/file_sigs.html
         'jpg' => "\xFF\xD8\xFF",
         'png' => "\x89\x50\x4E\x47\x0D\x0A\x1A\x0A",
         'gif' => "\x47\x49\x46"
     );
 
-    protected function matchesPattern($data, $pattern)
+    /**
+     * Default file type for this image if the header type could not be parsed
+     * @var string
+     */
+    public static $default_type = 'jpg';
+
+    /**
+     * Determines whether or not the current data segment contains the specified header string
+     * @param string $header Binary header string to check against
+     * @return boolean Flag indicating if this header is present
+     */
+    protected function hasDataHeader($header)
     {
-        return strpos($data, $pattern) === 0;
+        $data = $this->ImageData;
+        return strpos($data, $header) === 0;
     }
 
-
-    protected function determineFormat($data)
+    /**
+     * Determines the file format of the currently given 
+     * @return string 
+     */
+    protected function determineFormat()
     {
         foreach (self::$header_types as $type => $pattern)
-            if ($this->matchesPattern($data, $pattern))
+            if ($this->hasDataHeader($pattern))
                 return $type;
-        return 'jpg';
+        return self::$default_type;
     }
 
+    /**
+     * Determines if this image has valid content
+     * @return boolean A flag indicating the present of image data
+     */
     public function exists()
     {
         return !!$this->ImageData;
     }
 
-    public function setData($data)
+    /**
+     * Generate an appropriate random image filename for this file with appropriate extension
+     * @return string A random filename
+     */
+    protected function generateFilename()
+    {
+        $format = $this->determineFormat();
+        $name = '';
+        for ($i = 0; $i < 7; $i++)
+            $name .= chr(rand(97, 122));
+        return "image-data-$name-" . time() . ".$format";
+    }
+
+    function getFilename()
+    {
+        // Restores the Filename property to the default dataobject behaviour
+        return $this->getField('Filename');
+    }
+
+    function setParentID($parentID)
+    {
+        // Restores the ParentID property to the default dataobject behaviour
+        $this->setField('ParentID', $parentID);
+        return $this->getField('ParentID');
+    }
+
+    /**
+     * Safely assigns a binary string of data to this image with an optional filename
+     * @param string $data Binary string of image data
+     * @param string $filename Filename to use when storing this image internally
+     */
+    public function setData($data, $filename = null)
     {
         $this->ImageData = $data;
-        
-        $format = $this->determineFormat($data);
         $this->ParentID = -1;
-        $this->Filename = "image.$format";
+
+        // Set filename if requested, or there is currently no filename set
+        if ($filename || empty($this->Filename))
+            $this->Filename = $filename
+                    ? $filename
+                    : $this->generateFilename();
     }
-    
+
+    /**
+     * Retrieves image data
+     * @return type 
+     */
+    public function getData()
+    {
+        return $this->ImageData;
+    }
+
+    /**
+     * Extracts the image data from a given image as a binary string
+     * @param Image $image The image to import data from
+     * @return string|null A binary string containing the contained image if given
+     */
     protected function extractDataFromImage(Image $image)
     {
         // We'll need to extract content from the file itself
         if (!$image || !$image->exists())
             return null;
-        
-        if(is_a($image, 'MemoryImage'))
+
+        if (is_a($image, 'MemoryImage'))
             return $image->ImageData;
-        
+
         $path = $image->getFullPath();
-        return $this->extractDataFromFile($path);
-    }
-    
-    protected function extractDataFromFile($path)
-    {
-        if (!file_exists($path))
-            return null;
-        
-        return file_get_contents($path);
+        return $this->extractDataFromResource($path);
     }
 
-    public function CopyFromImage(Image $image)
+    /**
+     * Extracts data from a file or url resource path
+     * @param string $resource The full system path or url to the image
+     * @return string|null A binary string containing the file contents, or null if failure
+     */
+    protected function extractDataFromResource($resource)
+    {
+        $result = file_get_contents($resource);
+        if ($result === false)
+            return null;
+        return $result;
+    }
+
+    /**
+     * Copies the supplied image into this image
+     * @param Image $image the image object to copy from
+     * @param string $filename Optional filename to use when storing this image internally
+     * @return MemoryImage Reference to the resulting image object
+     */
+    public function CopyFromImage(Image $image, $filename = null)
     {
         $data = $this->extractDataFromImage($image);
-        $this->setData($data);
-    }
-    
-    public function CopyFromFile($path)
-    {
-        $data = $this->extractDataFromFile($path);
-        $this->setData($data);
+        $this->setData($data, $filename);
+        return $this;
     }
 
-    public function setDataBase64($data)
+    /**
+     * Writes the image data to the specified filesystem path
+     * @param string $path Output path to save this image data into
+     */
+    public function SaveToFile($path)
     {
-        $this->setData(base64_decode($data));
-    }    
-    
+        file_put_contents($path, $this->ImageData);
+    }
+
+    /**
+     * Copies the supplied file path into this image
+     * @param string $path The full system path to the image
+     * @param string $filename Optional filename to use when storing this image internally
+     * @return MemoryImage Reference to the resulting image object
+     */
+    public function CopyFromFile($path, $filename = null)
+    {
+        $data = $this->extractDataFromResource($path);
+        $this->setData($data, $filename);
+        return $this;
+    }
+
+    /**
+     * Load content from the provided image URL into memory
+     * @param string $url The URL of the image
+     * @param string $filename Optional filename to use when storing this image internally
+     * @return MemoryImage Reference to the resulting image object
+     */
+    public function CopyFromURL($url, $filename = null)
+    {
+        // This function really only exists as a convenience to those using it
+        return $this->CopyFromFile($url, $filename);
+    }
+
+    /**
+     * Assigns a base64 encoded string of data to this image with an optional filename
+     * @param string $data base64 encoded string of image data
+     * @param string $filename Filename to use when storing this image internally
+     */
+    public function setDataBase64($data, $filename = null)
+    {
+        $this->setData(base64_decode($data), $filename = null);
+    }
+
+    /**
+     * Retrieves the contents of this image as base64 encoded string
+     * @return string Base 64 encoded string containing this image data
+     */
+    public function getDataBase64()
+    {
+        return base64_encode($this->ImageData);
+    }
+
+    /**
+     * Ensures that any physically uploaded image assigned to this object is correctly loaded into the database
+     * before saving it 
+     */
     protected function onBeforeWrite()
     {
         parent::onBeforeWrite();
-        
+
         // if we have been assigned a file in the filesystem, before we save this record we should load this file
-        if($this->ParentID >= 0 && !empty($this->Name))
-            $this->CopyFromFile($this->getFullPath());
-        
-        // Required to prevent deletion of this during filesystem synctasks
+        if ($this->ParentID >= 0 && !empty($this->Name))
+        {
+            // Locate uploaded image
+            $path = $this->getFullPath();
+            $this->Filename = null; // Forces this image to be renamed
+            $this->CopyFromFile($path);
+
+            // remove physical file
+            if (self::$remove_uploaded_files)
+                unlink($path);
+        }
+
+        // Required to prevent deletion of this during filesystem synctasks, as well as a simple way of distinguishing
+        // this image from filesystem based images
         $this->ParentID = -1;
     }
 
@@ -105,7 +253,7 @@ class MemoryImage extends Image
      * @param string $format The name of the format.
      * @param string $arg1 An argument to pass to the generate function.
      * @param string $arg2 A second argument to pass to the generate function.
-     * @return Image_Cached
+     * @return MemoryImage The resulting memory image
      */
     function getFormattedImage($format, $arg1 = null, $arg2 = null)
     {
@@ -113,10 +261,10 @@ class MemoryImage extends Image
             return null;
 
         $data = $this->generateFormattedImage($format, $arg1, $arg2);
-        
-        if(!$data)
+
+        if (!$data)
             return null;
-        
+
         $sizedImage = new MemoryImage();
         $sizedImage->setData($data);
 
@@ -130,11 +278,11 @@ class MemoryImage extends Image
      * @param string $format Name of the format to generate.
      * @param string $arg1 Argument to pass to the generate method.
      * @param string $arg2 A second argument to pass to the generate method.
-     * @return binary Contents of the formatted image
+     * @return string String of binary contents of the formatted image
      */
     function generateFormattedImage($format, $arg1 = null, $arg2 = null)
     {
-        $gd = new MemoryGD($this->ImageData, $this->determineFormat($this->ImageData));
+        $gd = new MemoryGD($this->ImageData, $this->determineFormat());
         if (!$gd->hasGD())
             return null;
 
@@ -149,19 +297,21 @@ class MemoryImage extends Image
 
         USER_ERROR("MemoryImage::generateFormattedImage - Image $format function not found.", E_USER_WARNING);
     }
-    
+
     /**
      * Retrieve image data in a format suitable for putting into the src tag of an image element 
+     * @return string Embedded base64 encoded image in a format suitable for putting inside a html image src tag
      */
-	function getURL() {
-		if (!$this->ImageData)
+    function getURL()
+    {
+        if (!$this->exists())
             return null;
 
         $data = chunk_split(base64_encode($this->ImageData));
-        $format = $this->determineFormat($this->ImageData);
-        
+        $format = $this->determineFormat();
+
         return "data:image/$format;base64,$data";
-	}
+    }
 
     /**
      * Return an XHTML img tag for this Image,
@@ -172,7 +322,7 @@ class MemoryImage extends Image
     function getTag()
     {
         $data = $this->getURL();
-        if(!$data)
+        if (!$data)
             return null;
 
         if ($this->Title)
@@ -181,8 +331,18 @@ class MemoryImage extends Image
             $title = Convert::raw2att($matches[1]);
         else
             $title = $this->Filename;
-        
+
         return "<img src=\"$data\" alt=\"$title\" />";
+    }
+
+    public function deleteFormattedImages()
+    {
+        return 0;
+    }
+
+    public function updateFilesystem()
+    {
+        // Since there's no longer a filesystem we should no longer do this dance
     }
 
 }
